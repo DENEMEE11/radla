71cd410d046de024a736d2e8b932e60b44e45dbc
DENEMEE11 <97140083+DENEMEE11@users.noreply.github.com>
2023-08-21 02:32:27 +0300

    main.py

--- main.py
+++ main.py
@@ -6,13 +6,10 @@ import requests
 from colorama import init, Fore, Back, Style
 import lxml.html as lh
 from concurrent.futures import ThreadPoolExecutor
-import ctypes
-
-# Discord Webhook URL
-discord_webhook_url = "https://discordapp.com/api/webhooks/1130949638413881514/kRffi9U-XN9lPC1lgn6P0DAYTJep_xprGzSY0cPuO8RMnVtKnWC5moQMA7OZbYy4n8ME"
 
 # Set the title of the console window
-ctypes.windll.kernel32.SetConsoleTitleW("BTG Wallet Miner - Checked: "" | EARN: 0 BTG ~0₺" )
+def set_console_title(title):
+    print("\033]0;" + title + "\007", end="")  # This sets the terminal title
 
 init()
 
@@ -25,8 +22,6 @@ def get_bal(addr):
     return bal
 
 def process_address(hex64):
-    global counter, total_btg
-    
     PRIVATE_KEY: str = hex64
     hdwallet: HDWallet = HDWallet(symbol=SYMBOL)
     hdwallet.from_private_key(private_key=PRIVATE_KEY)
@@ -38,30 +33,17 @@ def process_address(hex64):
         print(Fore.RED, 'ADRES:', str(addr), 'PRİVATE KEY:', str(priv), 'BALLANCE:', str(xtxid), Style.RESET_ALL)
     else:
         print(Fore.GREEN, 'ADRES:', str(addr), 'PRİVATE KEY:', str(priv), 'BALLANCE:', str(xtxid), Style.RESET_ALL)
-        print("devam etmek için enter e basın")
 
-    ifer = '0 BTG'
-    if str(xtxid) != str(ifer):
-        hit_info = f"ADRES: {addr} PRİVATE KEY: {priv} BALLANCE: {xtxid}\n"
-        with open("hits.txt", "a") as f:
-            f.write(hit_info)
-            f.write("PRİVATE KEY = " + str(priv))
-            f.write("\n================[DΞİTY#5637]================")
-            
-        total_btg += int(xtxid.split()[0])
-        total_btga = total_btg * 386
-
-            
-                
-    counter += 1
-    total_btga = (total_btg) * 386 
-    ctypes.windll.kernel32.SetConsoleTitleW("BTG Wallet Miner - Checked: " + str(counter) + " | EARN: " + str(total_btg) + "BTG ~" + str(total_btga) + "₺")
-
+        ifer = '0 BTG'
+        if str(xtxid) != str(ifer):
+            hit_info = f"ADRES: {addr} PRİVATE KEY: {priv} BALLANCE: {xtxid}\n"
+            with open("hits.txt", "a") as f:
+                f.write(hit_info)
+                f.write("PRİVATE KEY = " + str(priv))
+                f.write("\n================[DΞİTY#5637]================")
 
 def main():
-    global counter, total_btg
-    counter = 0
-    total_btg = 0
+    set_console_title("BTG Wallet Miner - Checked: 0 | EARN: 0 BTG ~ 0₺")
 
     with ThreadPoolExecutor(max_workers=50) as executor:
         while True:
